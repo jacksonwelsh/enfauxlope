25610f1374f5d6bc1a9be542d61a58cd62648fd7
Austin Leath <austinleath18@gmail.com>
2022-04-24 01:33:42 -0500

    commit fix

--- web/src/Transactions.jsx
+++ web/src/Transactions.jsx
@@ -6,16 +6,34 @@ const Transactions = () => {
   const [category, setCategoryName] = React.useState(undefined);
 
   React.useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_ROOT}/cards/transactions/category/` + params.category)
+    fetch(
+      `${process.env.REACT_APP_API_ROOT}/cards/transactions/category/` +
+        params.category
+    )
       .then((r) => r.json())
       .then((t) => setCategoryName(t));
   }, [params.category]);
+
+  
+
+
+console.log(category?.transactions.map((t) => t.amount));
 
 
   return (
     <>
       <div className="container mx-auto my-4">
-        <h1 className="text-6xl font-black">{category?.extname} Transactions</h1>
+        <h1 className="text-6xl font-black">
+          {category?.extname} Transactions
+        </h1>
+        <br/>
+        <h2 className="text-2xl font-black">
+          Total spent:{" $"}
+          {Object.values(category?.transactions ?? {}).reduce(
+            (prev, curr) => (prev += curr.amount /100),
+            0
+          )}
+        </h2>
       </div>
     </>
   );

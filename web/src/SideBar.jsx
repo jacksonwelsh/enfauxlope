9efc866a2610ba34552f54c3ed7ce60316ccc91d
Jackson Welsh <me@jacksonwel.sh>
2022-04-24 04:13:30 -0500

    responsive sidebar

---
+++ web/src/SideBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SideBar = () => {
+  return (
+    <div className="bg-gray-900 z-10 hidden md:block md:w-64 lg:w-80 mr-6 h-screen p-4">
+      <h2 className="font-bold text-4xl">Your Activity</h2>
+    </div>
+  );
+};
+
+export default SideBar;

eae905cb8c5212b3f55bd733d9ecaa32572ac4ff
Jackson Welsh <me@jacksonwel.sh>
2022-04-24 03:17:33 -0500

    pretty background

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -6,8 +6,8 @@ import Transactions from "./Transactions";
 
 const App = () => {
   return (
-    <Router className="bg-gray-900 text-gray-50">
-      <div className="flex w-full h-8 px-4 py-1 font-mono bg-black justify-between items-center">
+    <Router className="text-gray-50">
+      <div className="z-10 flex w-full h-8 px-4 py-1 font-mono bg-black justify-between items-center">
         <Link
           to="/"
           className="font-bold tracking-wider bg-gradient-to-tr from-blue-400 to-purple-400 bg-clip-text text-transparent uppercase"
@@ -23,12 +23,17 @@ const App = () => {
           </Link>
         </div>
       </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<CreateLimit />} />
-        <Route path="/card" element={<CardDetails />} />
-        <Route path="/categories/:category" element={<Transactions />} />
-      </Routes>
+      <div className="z-10">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<CreateLimit />} />
+          <Route path="/card" element={<CardDetails />} />
+          <Route path="/categories/:category" element={<Transactions />} />
+        </Routes>
+      </div>
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
     </Router>
   );
 };
